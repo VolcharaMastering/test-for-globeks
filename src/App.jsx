8e7adbffb2b4dbd0a9b071198f3110584977d4ee
Aleksey <vmstr@vmstr.ru>
2023-10-28 00:20:24 +0200

    start of task

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import "./App.scss";
+
+function App() {
+  // const [popupOpen, setpopupOpen] = useState(false);
+
+  return (
+    <>
+      <div></div>
+      
+    </>
+  );
+}
+
+export default App;
