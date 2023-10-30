46475dffe5af2c0a2ec6db6511333b469b7d86d0
Aleksey <vmstr@vmstr.ru>
2023-10-31 00:38:16 +0100

    Test2. Bugfix & test on server. screens added

--- task2/request.sql
+++ task2/request.sql
@@ -1,38 +1,61 @@
+use testdb;
 
--- !!!!!!!!!!!!
--- по неизвестной причине зафейлилась установка sql сервера. Могу приложить только этот скрипт с описанием.
--- !!!!!!!!!!!!
-WITH RecSubs AS (
-    -- выбираем данные по сотруднику, от которого стартует поиск, sub_level делаем 0 
-    SELECT 
-        id AS sub_id,
-        name AS sub_name,
-        parent_id,
-        0 AS sub_level
-    FROM subdivisions
-    WHERE id = 710253
-    
-    UNION ALL
-    -- собираем все подразделения "ниже", формируем "вложенность" и убираем 100055, 100059
-    SELECT 
-        s.id AS sub_id,
-        s.name AS sub_name,
-        s.parent_id,
-        rs.sub_level + 1 AS sub_level
-    FROM subdivisions s
-    INNER JOIN RecSubs rs ON s.parent_id = rs.sub_id
-    WHERE s.id NOT IN (100055, 100059) 
-)
-    -- Выбираем всех, кто подходит под наши условия и формируем окончательную таблицу. Упорядочиваем по вложенности
-    SELECT 
+SET
+    STATISTICS TIME ON;
+
+GO
+    WITH RecSubs AS (
+        --- Общяя таблицы выражений (CTE) с именем "RecSubs",
+        SELECT
+            s.id AS sub_id,
+            s.name AS sub_name,
+            s.parent_id,
+            0 AS sub_level
+        FROM
+            subdivisions s --- Данные из таблицы "subdivisions" и используем ее алиас "s".
+        WHERE
+            s.id = (
+                SELECT
+                    subdivision_id
+                FROM
+                    collaborators
+                WHERE
+                    id = 710253
+            ) --- Фильтр. Строки из таблицы подразделений, где ID = подразделению сотрудника с ID 710253.
+        UNION
+        ALL
+        SELECT
+            s.id,
+            s.name,
+            s.parent_id,
+            rs.sub_level + 1
+        FROM
+            subdivisions s
+            INNER JOIN RecSubs rs ON s.parent_id = rs.sub_id --- Объединение текущей строки таблицы "subdivisions" с результатами (CTE "RecursiveSubs") по ID родительского подразделения.
+        WHERE
+            s.id NOT IN (100055, 100059)
+    )
+SELECT
     c.id,
     c.name,
     rs.sub_name,
     rs.sub_id,
     rs.sub_level,
-    COUNT(*) OVER (PARTITION BY rs.sub_id) AS colls_count
-FROM collaborators c
-INNER JOIN RecSubs rs ON c.subdivision_id = rs.sub_id
-WHERE c.age < 40
-AND LEN(c.name) > 11
-ORDER BY rs.sub_level;
+    (
+        SELECT
+            COUNT(*)
+        FROM
+            collaborators
+        WHERE
+            subdivision_id = rs.sub_id
+    ) AS colls_count ---  подзапрос, считает общее количество сотрудников в текущем подразделении, чей ID подразделения совпадает с ID текущего подразделения из CTE.
+FROM
+    collaborators c
+    JOIN RecSubs rs ON c.subdivision_id = rs.sub_id ---   Объединение данных таблицы "collaborators" с результатами CTE по ID подразделения.
+WHERE
+    c.age < 40
+    AND LEN(c.name) > 11
+ORDER BY
+    rs.sub_level;
+
+GO
